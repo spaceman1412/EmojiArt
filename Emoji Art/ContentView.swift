0ec065d788fc96a0f6b309d8a0d604dee2572a94
spaceman1412 <spacemannormal@gmail.com>
2025-01-02 11:47:26 +0700

    Initial Commit

---
+++ Emoji Art/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Emoji Art
+//
+//  Created by H470-088 on 2/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
